025589f72c19f06d614d41761ded09f85c458a9a
zizwiz <zizwiz@engineer.com>
2026-07-06 13:58:07 +0100

    Initial Upload
    Initial upload of empty solution

---
+++ myEnergyUsage/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace myEnergyUsage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
